35d44746425fd40e2b5c59f393c577e640f6cb35
andre <andremaivelasco@hotmail.com>
2023-06-15 19:39:32 -0300

    condutor client criada

--- src/client/CondutorClient.ts
+++ src/client/CondutorClient.ts
@@ -6,14 +6,59 @@ export class CondutorClient{
 
     constructor(){
         this.axiosClient = axios.create({
-            baseURL: "http://localhost:8080/api/condutor",
+            baseURL: "http://localhost:8080/api",
             headers: {"Content-type" : "application/json"}
         });
     }
 
     public async findById(id : number) : Promise<Condutor> {
         try{
-            return (await this.axiosClient.get<Condutor>(`?id=${id}`)).data;
+            return (await this.axiosClient.get<Condutor>(`/condutor?id=${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAll() : Promise<Condutor[]> {
+        try{
+            return (await this.axiosClient.get<Condutor[]>(`/condutor/lista`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAtivo() : Promise<Condutor[]> {
+        try{
+            return (await this.axiosClient.get<Condutor[]>(`/condutor/ativos`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async cadastrar(condutor : Condutor) : Promise<void> {
+        try{
+            return (await this.axiosClient.post(`/condutor`, condutor)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async editar(id : number, condutor : Condutor) : Promise<void> {
+        try{
+            return (await this.axiosClient.put(`/condutor?id=${id}`, condutor)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async excluir(id : number) : Promise<void> {
+        try{
+            return (await this.axiosClient.delete(`/condutor?id=${id}`)).data;
         }
         catch(error : any){
             return Promise.reject(error.response);
