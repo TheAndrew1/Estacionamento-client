ba4a77d2d917d0342db83eae438d55e52b5cc412
andre <andremaivelasco@hotmail.com>
2023-06-15 19:43:46 -0300

    marca client criada

---
+++ src/client/MarcaClient.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosInstance } from "axios";
+import { Marca } from "@/model/marca";
+
+export class CondutorClient{
+    private axiosClient : AxiosInstance;
+
+    constructor(){
+        this.axiosClient = axios.create({
+            baseURL: "http://localhost:8080/api",
+            headers: {"Content-type" : "application/json"}
+        });
+    }
+
+    public async findById(id : number) : Promise<Marca> {
+        try{
+            return (await this.axiosClient.get<Marca>(`/marca?id=${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAll() : Promise<Marca[]> {
+        try{
+            return (await this.axiosClient.get<Marca[]>(`/marca/lista`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async findAtivo() : Promise<Marca[]> {
+        try{
+            return (await this.axiosClient.get<Marca[]>(`/marca/ativos`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async cadastrar(marca : Marca) : Promise<void> {
+        try{
+            return (await this.axiosClient.post(`/marca`, marca)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async editar(id : number, marca : Marca) : Promise<void> {
+        try{
+            return (await this.axiosClient.put(`/marca?id=${id}`, marca)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async excluir(id : number) : Promise<void> {
+        try{
+            return (await this.axiosClient.delete(`/marca?id=${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+}
