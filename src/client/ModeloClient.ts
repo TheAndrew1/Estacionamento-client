472f4589a87a9588698588868b030d5765b188b1
andre <andremaivelasco@hotmail.com>
2023-06-24 19:21:54 -0300

    estrutura do formulário de modelo pronta, falta ajustar o envio da marca

--- src/client/ModeloClient.ts
+++ src/client/ModeloClient.ts
@@ -38,27 +38,27 @@ export class ModeloClient{
         }
     }
 
-    public async cadastrar(modelo : Modelo) : Promise<void> {
+    public async cadastrar(modelo : Modelo) : Promise<string> {
         try{
-            return (await this.axiosClient.post(`/modelo`, modelo)).data;
+            return (await this.axiosClient.post<string>(`/modelo`, modelo)).data;
         }
         catch(error : any){
             return Promise.reject(error.response);
         }
     }
 
-    public async editar(id : number, modelo : Modelo) : Promise<void> {
+    public async editar(id : number, modelo : Modelo) : Promise<string> {
         try{
-            return (await this.axiosClient.put(`/modelo?id=${id}`, modelo)).data;
+            return (await this.axiosClient.put<string>(`/modelo?id=${id}`, modelo)).data;
         }
         catch(error : any){
             return Promise.reject(error.response);
         }
     }
 
-    public async excluir(id : number) : Promise<void> {
+    public async excluir(id : number) : Promise<string> {
         try{
-            return (await this.axiosClient.delete(`/modelo?id=${id}`)).data;
+            return (await this.axiosClient.delete<string>(`/modelo?id=${id}`)).data;
         }
         catch(error : any){
             return Promise.reject(error.response);
--- src/views/modelo/ModeloFormView.vue
+++ src/views/modelo/ModeloFormView.vue
@@ -1,36 +1,129 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ModeloClient from '@/client/ModeloClient';
+import MarcaClient from '@/client/MarcaClient';
+import { Modelo } from '@/model/modelo';
+import { Marca } from '@/model/marca';
 
 export default defineComponent({
-  name: 'FormModelo'
+  name: 'FormModelo',
+  data() {
+    return {
+      modelo: new Modelo(),
+      id: this.$route.query.id,
+      form: this.$route.query.form,
+      alert: {
+        active: false as boolean,
+        message: '' as string,
+        class: '' as string
+      },
+      marcas: new Array<Marca>()
+    };
+  },
+  methods: {
+    findById(id: number) {
+      ModeloClient.findById(id).then(response => (this.modelo = response));
+    },
+    onClickCadastrar(modelo: Modelo) {
+      if (!this.alert.active) {
+        this.alert.active = !this.alert.active;
+      }
+
+      ModeloClient.cadastrar(modelo)
+        .then(success => {
+          this.alert.message = success;
+          this.alert.class = 'alert alert-dismissible alert-success';
+        })
+        .catch(error => {
+          this.alert.message = error.data;
+          this.alert.class = 'alert alert-dismissible alert-danger';
+        });
+    },
+    onClickEditar(id: number, modelo: Modelo) {
+      if (!this.alert.active) {
+        this.alert.active = !this.alert.active;
+      }
+
+      ModeloClient.editar(id, modelo)
+        .then(success => {
+          this.alert.message = success;
+          this.alert.class = 'alert alert-dismissible alert-success';
+        })
+        .catch(error => {
+          this.alert.message = error.data;
+          this.alert.class = 'alert alert-dismissible alert-danger';
+        });
+    },
+    onClickExcluir(id: number) {
+      if (!this.alert.active) {
+        this.alert.active = !this.alert.active;
+      }
+
+      ModeloClient.excluir(id)
+        .then(success => {
+          this.alert.message = success;
+          this.alert.class = 'alert alert-dismissible alert-success';
+        })
+        .catch(error => {
+          this.alert.message = error.data;
+          this.alert.class = 'alert alert-dismissible alert-danger';
+        });
+    },
+    onClickFechar() {
+      this.alert.active = !this.alert.active;
+      this.alert.message = '';
+      this.alert.class = '';
+    },
+    findMarcas(){
+      MarcaClient.findAll()
+        .then(success => {
+          this.marcas = success;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+    if (this.id != undefined) {
+      this.findById(Number(this.id));
+    }
+
+    this.findMarcas();
+  }
 });
 </script>
 
 <template>
   <h2>Cadastrar Modelo</h2>
 
+  <div class="container" :class="alert.class" role="alert" v-if="alert.active">
+    <div>{{ alert.message }}</div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" @click="onClickFechar"></button>
+  </div>
+
   <form>
     <div class="container">
       <div class="row justify-content-center my-3">
         <label for="nome">Nome:</label>
-        <input type="text" class="form-control w-25" id="nome" placeholder="Nome do modelo">
+        <input type="text" class="form-control w-25" id="nome" placeholder="Nome do modelo" v-model="modelo.nome" :disabled="form == 'excluir'" autocomplete="off">
       </div>
       <div class="row justify-content-center my-3">
         <label for="marca">Marca:</label>
-        <select class="form-select w-25" id="marca">
-          <option selected disabled>Marca</option>
-          <option value="1">Hyundai</option>
-          <option value="2">Chevrolet</option>
-          <option value="3">Renault</option>
-        </select>
+        <input class="form-control w-25" list="marcas" placeholder="Nome da marca">
+        <datalist id="marcas">
+          <option v-for="marca in marcas" :key="marca.id" :value="marca.nome"></option>
+        </datalist>
       </div>
       <div class="row justify-content-center">
         <div class="col">
-            <button type="submit" class="btn btn-success rounded-0">Cadastrar</button>
+          <button v-if="form === undefined" type="button" class="btn btn-outline-success rounded-0" @click="onClickCadastrar(modelo)">Cadastrar</button>
+          <button v-if="form === 'editar'" type="button" class="btn btn-outline-warning rounded-0" @click="onClickEditar(Number(id), modelo)">Editar</button>
+          <button v-if="form === 'excluir'" type="button" class="btn btn-outline-danger rounded-0" @click="onClickExcluir(Number(id))">Excluir</button>
         </div>
-        
+            
         <div class="col">
-            <router-link to="/listar-modelo"><button type="button" class="btn btn-outline-success rounded-0">Voltar</button></router-link>
+          <router-link to="/listar-modelo" type="button" class="btn btn-outline-secondary rounded-0">Voltar</router-link>
         </div>
       </div>
     </div>
@@ -38,7 +131,7 @@ export default defineComponent({
 </template>
 
 <style scoped>
-  th, td{
+  th, td {
     vertical-align: middle;
   }
 </style>
